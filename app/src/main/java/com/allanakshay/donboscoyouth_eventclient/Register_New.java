09883e55a350bbbb15b58d7aac47881d08b180af
allanakshay12 <allanakshay@outlook.com>
2017-10-04 20:40:10 +0530

    Extra Content

--- app/src/main/java/com/allanakshay/donboscoyouth_eventclient/Register_New.java
+++ app/src/main/java/com/allanakshay/donboscoyouth_eventclient/Register_New.java
@@ -1,6 +1,5 @@
 package com.allanakshay.donboscoyouth_eventclient;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,28 +7,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class Cashier_activity extends AppCompatActivity {
-    Intent intent;
+public class Register_New extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cashier_activity);
+        setContentView(R.layout.activity_register__new);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
-    }
-
-    public void Register_New_User(View view)
-    {
-        intent = new Intent(Cashier_activity.this, Register_New.class);
-        startActivity(intent);
-    }
-    public void Recharge_Existing_Account(View view)
-    {
-        intent = new Intent(Cashier_activity.this, Recharge_Existing.class);
-        startActivity(intent);
     }
 
 }
